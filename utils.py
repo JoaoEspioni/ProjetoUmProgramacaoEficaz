89c27ed758747f772b49e4fd8dc99d84feb07f29
joao nunes <joaopedrosenunes@gmail.com>
2026-08-14 10:32:50 -0300

    Handout parte 3 concluido

--- utils.py
+++ utils.py
@@ -1,5 +1,18 @@
 import json
-def load_data():
-    with open('static/data/notes.json', 'r', encoding='utf-8') as file:
+from pathlib import Path
+
+
+def load_data(nome_arquivo='notes.json'):
+    caminho_arquivo = Path(__file__).resolve().parent / 'static' / 'data' / nome_arquivo
+    with open(caminho_arquivo, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
+
+
+def load_template(nome_template):
+    base_dir = Path(__file__).resolve().parent
+    caminho_template = base_dir / 'static' / 'templates' / nome_template
+
+    with open(caminho_template, 'r', encoding='utf-8') as file:
+        template = file.read()
+    return template
---
+++ views.py
@@ -0,0 +1,12 @@
+from utils import load_data, load_template
+from flask import render_template_string
+
+def index():
+    note_template = load_template('components/note.html')
+    notes_li = [
+        note_template.format(title=dados['titulo'], details=dados['detalhes'])
+        for dados in load_data('notes.json')
+    ]
+    notes = '\n'.join(notes_li)
+
+    return render_template_string(load_template('index.html'), notes=notes)
